e62004de2de6a58ad941f72aae4bd60fcc9cdb13
sreerag-nair <sreerag@kunato.io>
2024-03-06 14:50:26 +0530

    working on the next util function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -13,6 +13,7 @@ pub enum FileReadingReturnTypes {
     ReturnBufferHandle(BufReader<File>),
 }
 
+#[derive(Debug)]
 pub enum JSONErrorTypes {
     JSONError(serde_json::Error),
     Error(Error),
@@ -63,31 +64,37 @@ impl Utils {
         // }
     }
 
-    pub fn write_to_file(path_to_file: &str, panic_if_absent: bool) {}
+    // pub fn write_to_file(path_to_file: &str, panic_if_absent: bool) {}
 
     // TODO: UNCOMMENT THIS AFTER WRITING THE TEST CASES
-    // pub fn parse_to_json<'a, json_blue_print>(path_to_file: &str) -> Result<_, JSONErrorTypes>
-    // where
-    //     json_blue_print: Deserialize<'a>,
-    // {
-    //     let contents_to_string = Utils::read_file(path_to_file, true, false)?;
+    pub fn parse_to_json<json_blue_print>(
+        path_to_file: &str,
+    ) -> Result<json_blue_print, JSONErrorTypes>
+    where
+        json_blue_print: for<'a> Deserialize<'a>,
+    {
+        let file_handle = Utils::read_file(path_to_file, true, false).unwrap();
 
-    //     let mut contents_to_json = match contents_to_string {
-    //         FileReadingReturnTypes::ReturnBufferHandle(buffer) => {
-    //             let mut m: String;
-    //             for line in buffer.lines() {
-    //                 m.push_str(&line.unwrap());
-    //             }
+        let file_contents = match file_handle {
+            FileReadingReturnTypes::ReturnBufferHandle(buffer) => {
+                let mut string_collector: String = String::new();
+                for line in buffer.lines() {
+                    string_collector.push_str(&line.unwrap());
+                }
 
-    //             serde_json::from_str(&m)
-    //         }
-    //         FileReadingReturnTypes::ReturnEntireFile(file_as_string) => {
-    //             serde_json::from_str(&file_as_string)
-    //         }
-    //     };
+                string_collector
+            }
+            FileReadingReturnTypes::ReturnEntireFile(file_as_string) => {
+                // serde_json::from_str(&file_as_string)
+                file_as_string
+            }
+        };
 
-    //     contents_to_json
-    // }
+        match serde_json::from_str::<json_blue_print>(&file_contents) {
+            Ok(deserialized_file) => Ok(deserialized_file),
+            Err(err) => Err(JSONErrorTypes::JSONError(err)),
+        }
+    }
 }
 
 // test cases
@@ -103,7 +110,9 @@ impl Utils {
 #[cfg(test)]
 mod tests {
 
-    use std::str::FromStr;
+    use std::panic;
+
+    use serde::Serialize;
 
     use super::*;
 
@@ -113,12 +122,7 @@ mod tests {
     static PERMISSION_DENIED_FILE_LOCATION: &str = "";
     static PERMISSION_DENIED_FOLDER_LOCATION: &str = "";
 
-    // #[test]
-    // #[should_panic(expected = "Error while opening the file")]
-    // // #[cfg(test)]
-    // fn read_from_an_invalid_location() {
-    //     Utils::read_file(INVALID_FILE_LOCATION, true, true);
-    // }
+    static VALID_JSON_FILE_LOCATION: &str = "/home/wwwabcomin/codes/rn/test_json.json";
 
     #[test]
     #[should_panic(expected = "Error while opening the file")]
@@ -197,4 +201,66 @@ mod tests {
             }
         }
     }
+
+    #[test]
+    #[should_panic(expected = "Error while opening the file")]
+
+    fn read_from_folder() {
+        let read_file_result: Result<FileReadingReturnTypes, Error> =
+            Utils::read_file(FOLDER_LOCATION, true, false);
+
+        match read_file_result {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+    }
+
+    #[test]
+    fn try_reading_a_permission_denied_file() {}
+
+    #[test]
+    fn try_reading_a_permission_denied_folder() {}
+
+    // tc for writing to a file
+
+    // #[test]
+    // fn write_to_a_valid_file() {}
+
+    // #[test]
+    // fn write_to_a_file_which_doesnt_exist() {}
+
+    // #[test]
+    // fn write_to_a_folder() {}
+
+    // #[test]
+    // fn write_to_a_readonly_file() {}
+
+    // #[test]
+    // fn write_to_an_invalid_file() {}
+
+    // tc for parsing to a json
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct X {
+        x: u32,
+    }
+
+    #[test]
+    fn parse_to_a_valid_json() {
+        let result = Utils::parse_to_json::<X>(VALID_JSON_FILE_LOCATION);
+
+        // println!("{:?}", result);
+
+        match result {
+            Ok(file) => {
+                panic!("{:?}", file);
+            }
+            Err(e) => {
+                panic!("{:?}", e)
+            }
+        }
+    }
+
+    // #[test]
+    // fn parse_to_an_invalid_json() {}
 }
